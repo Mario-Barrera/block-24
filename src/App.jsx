b1466d214178e89882126ee637ae6d634742422a
Mario Barrera <chocolate_4life@yahoo.com>
2025-06-11 21:17:48 -0500

    Add puppy data and display with unique keys in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import { puppyList } from './data.js';
+
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppies: "puppies);
+  
   return (
-    <div>
-      {/* Starting point for your app */}
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </div>
   );
 }
