172bfc68ceca754f5d865d4bf9496e9292741c37
Mario Barrera <chocolate_4life@yahoo.com>
2025-06-11 23:01:41 -0500

    Render detailed info for selected puppy

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,31 @@ import { puppyList } from './data.js';
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppies: "puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  console.log("puppies:", puppies);
+  console.log("Selected puppy ID:", featPupId);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featuredPup:", featuredPup);
   
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
+      {puppies.map(puppy => (
+        <p key={puppy.id} onClick={() => setFeatPupId(puppy.id)}>
+        {puppy.name}
+        </p>
+      ))}
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
